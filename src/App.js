3e8ea8416cc087ab97f2e1c097540d77e12eca55
Mar.io <mario@stake.co.jp>
2021-10-15 15:01:43 +0200

    template for react sunbstrate app

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState, createRef } from 'react';
+import { Dimmer, Loader, Grid, Message } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+import './App.css';
+import { SubstrateContextProvider, useSubstrate } from './substrate-lib';
+
+import NodeInfo from './NodeInfo';
+
+
+function Main () {
+  const [accountAddress] = useState(null);
+  const { apiState, keyring, keyringState, apiError } = useSubstrate();
+  const accountPair =
+    accountAddress &&
+    keyringState === 'READY' &&
+    keyring.getPair(accountAddress);
+
+  const loader = text =>
+    <Dimmer active>
+      <Loader size='small'>{text}</Loader>
+    </Dimmer>;
+
+  const message = err =>
+    <Grid centered columns={2} padded>
+      <Grid.Column>
+        <Message negative compact floating
+          header='Error Connecting to Substrate'
+          content={`${JSON.stringify(err, null, 4)}`}
+        />
+      </Grid.Column>
+    </Grid>;
+
+  if (apiState === 'ERROR') return message(apiError);
+  else if (apiState !== 'READY') return loader('Connecting to Substrate');
+
+  if (keyringState !== 'READY') {
+    return loader('Loading accounts (please review any extension\'s authorization)');
+  }
+
+  const contextRef = createRef();
+
+  return (
+    <div className="App">
+      <Grid stackable columns='equal'>
+          <Grid.Row stretched>
+              <NodeInfo/>
+          </Grid.Row>
+      </Grid>
+    </div>
+  );
+}
+
+export default function App () {
+  return (
+    <SubstrateContextProvider>
+      <Main />
+    </SubstrateContextProvider>
+  );
+}
